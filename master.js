42a660c6a00d24e75d135738d8daa373a08a2813
Nicolás Di virgilio <nicolas_dv93@outlook.com>
2025-08-07 20:54:51 -0300

    function con map y split

---
+++ master.js
@@ -0,0 +1,17 @@
+
+//convertir una frase con guiones a una cadena de palabras en la cual esten juntas y comiencen en mayuscula
+function palabra(str){
+    let separacion= str.split("-")
+    let mapeo= separacion.map((index, letras)=>{
+        if (index===0) {
+            return letras;
+        }
+      
+        return letras.charAt(0).toUpperCase() + letras.slice(1)
+    })
+return mapeo.join('')
+}
+
+console.log(palabra("hola-mundo"));
+
+
