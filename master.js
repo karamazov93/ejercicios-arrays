db88ca208200a32e36c56861f09e7bf73f186d6b
Nicolás Di virgilio <nicolas_dv93@outlook.com>
2025-08-07 21:02:18 -0300

    ejercicios

--- master.js
+++ master.js
@@ -2,11 +2,10 @@
 //convertir una frase con guiones a una cadena de palabras en la cual esten juntas y comiencen en mayuscula
 function palabra(str){
     let separacion= str.split("-")
-    let mapeo= separacion.map((index, letras)=>{
+    let mapeo= separacion.map((letras, index)=>{
         if (index===0) {
             return letras;
         }
-      
         return letras.charAt(0).toUpperCase() + letras.slice(1)
     })
 return mapeo.join('')
